e4253b17ab6fe341d9f64be9c03ade5dfb2b8182
VRomanenko04 <vovasan2004@gmail.com>
2023-01-31 23:41:30 +0200

    Плавный фон, папка анимаций, недоделанный js

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,17 @@
 let headerButton = document.querySelector('.menu-btn');
 let menuView = document.querySelector('.menu');
 let menuBackground = document.querySelector('.body');
+let logoImg = document.querySelector('.logo');
+let teemsImg = ["imgs/logo.png", "imgs/darkLogo.png"];
 
 headerButton.addEventListener('click' , function() {
     headerButton.classList.toggle('active');
     menuView.classList.toggle('active');
     menuBackground.classList.toggle('active');
 });
+
+function changeImg() {
+    for(let i=0; i<teemsImg.length; i++){
+        logoImg.src = teemsImg[i];
+    }
+};
